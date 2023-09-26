f85e84c2eba6a79c34e5cede8c11809ee1d2231d
Raul <raulgarcicespedes@gmail.com>
2023-09-26 21:48:56 +0200

    se ha añadido un paquete

--- src/main/Window.java
+++ src/main/Window.java
@@ -1,17 +1,19 @@
+package main;
+
 import javax.swing.JFrame;
-public class Main extends JFrame {
+public class Window extends JFrame {
 
     public static final int WIDTH = 800, HEIGHT = 600;              //Window dimensions
-    public Main(){                                                  //Constructor general
+    public Window(){                                                  //Constructor general
         setTitle("Survivor");                                       //Window title
         setSize(WIDTH,HEIGHT);                                      //Window dimensions
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             //Allows to close window when pressing x
         setResizable(false);                                        //Cant resize window
         setLocationRelativeTo(null);                                //Window appears in the middle of the screen
-
         setVisible(true);                                           //Window can now be seeing
+
     }
     public static void main(String[] args) {
-        new Main();
+        new Window();
     }
 }
